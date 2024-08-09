f655aa59f9fb913fb3695bcc9e1c98c7116bdab6
Anshul <anshulraturi007@gmail.com>
2024-08-09 20:06:53 +0530

    Fixed layout problem and multiple component display problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../globals.css";
+import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
@@ -16,22 +16,30 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      signInFallbackRedirectUrl={
+        process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL
+      }
+      signUpFallbackRedirectUrl={
+        process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL
+      }
+    >
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
-        <main className='flex flex-row'>
-          <Topbar />
-          <LeftSidebar />
-          <section className='main-container'>
-            <div className='w-full max-w-4xl'>{children}</div>
-          </section>
-          <RightSidebar />
-        </main>
-        <Bottombar />
+        <body className={inter.className}>
+          <main className='flex flex-row'>
+            <Topbar />
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
