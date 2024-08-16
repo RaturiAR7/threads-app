58ad844a9babd764fbe2d08cbdf20ee02436616b
Anshul <anshulraturi007@gmail.com>
2024-08-17 00:31:36 +0530

    Added loading between navigations

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,11 @@
+function loading() {
+  return (
+    <div>
+      <h2 className='text-white text-heading3-bold animate-pulse'>
+        Loading...
+      </h2>
+    </div>
+  );
+}
+
+export default loading;
